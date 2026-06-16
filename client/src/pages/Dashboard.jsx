f6bd171b19356d84e64248d35e4b51b5d4097872
200-hassan <md.arhassan@gmail.com>
2026-06-17 03:32:24 +0600

    create basic structure of the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react"
+import { dummyEmployeeDashboardData } from "../assets/assets";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    setData(dummyEmployeeDashboardData)
+    setTimeout(()=>{
+      setLoading(false)
+    },1000)
+  },[])
+
+  if(loading) return <p>Loading</p>
+  if(!data) return <p className="text-center text-slate-500 py-12">Failed to load dashboard</p>
+
+  if(data.role === "ADMIN"){
+    return <div>admin dashboard</div>
+  }else{
+    return <div>employee dashboard</div>
+  }
+
+  
 }
 
 export default Dashboard
