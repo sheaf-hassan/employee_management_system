aa743c33237a59c730f814602e8dab3137dcdb81
200-hassan <md.arhassan@gmail.com>
2026-06-18 02:52:28 +0600

    create the header of employees page

--- client/src/pages/Employees.jsx
+++ client/src/pages/Employees.jsx
@@ -1,35 +1,41 @@
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react";
 import { dummyEmployeeData } from "../assets/assets";
+import { Plus } from "lucide-react";
 
 const Employees = () => {
-
   const [employees, setEmployees] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const fetchEmployees = useCallback(async ()=>{
-    setLoading(true)
-    setEmployees(dummyEmployeeData)
-    setTimeout(()=>{
-      setLoading(false)
-    }, 1000)
-  }, [])
+  const fetchEmployees = useCallback(async () => {
+    setLoading(true);
+    setEmployees(dummyEmployeeData);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchEmployees();
-  },[])
+  }, []);
 
   return (
     <div className="animate-fade-in">
       {/* Header */}
-      <div>
-
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-8">
+        <div>
+          <h1 className="page-title">Employees</h1>
+          <p className="page-subtitle">Manage your team members</p>
+        </div>
+        <button className="btn-primary flex items-center gap-2 w-full sm:w-auto justify-center">
+          <Plus size={16}/> Add Employee
+        </button>
       </div>
 
       {/* Search bar */}
 
       {/* Employee card */}
     </div>
-  )
-}
+  );
+};
 
-export default Employees
+export default Employees;
