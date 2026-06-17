6c06ae79b37072089fd311f80f6a33b54c10f42b
200-hassan <md.arhassan@gmail.com>
2026-06-18 02:39:56 +0600

    create the employees page functions and structure

--- client/src/pages/Employees.jsx
+++ client/src/pages/Employees.jsx
@@ -1,7 +1,34 @@
+import { useCallback, useEffect, useState } from "react"
+import { dummyEmployeeData } from "../assets/assets";
 
 const Employees = () => {
+
+  const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchEmployees = useCallback(async ()=>{
+    setLoading(true)
+    setEmployees(dummyEmployeeData)
+    setTimeout(()=>{
+      setLoading(false)
+    }, 1000)
+  }, [])
+
+  useEffect(()=>{
+    fetchEmployees();
+  },[])
+
   return (
-    <div>Employees</div>
+    <div className="animate-fade-in">
+      {/* Header */}
+      <div>
+
+      </div>
+
+      {/* Search bar */}
+
+      {/* Employee card */}
+    </div>
   )
 }
 
