96905b31bf280de147636c311a885d31cb7f75ba
200-hassan <md.arhassan@gmail.com>
2026-06-17 16:14:18 +0600

    create buttons of employee_dashboard

--- client/src/components/EmployeeDashboard.jsx
+++ client/src/components/EmployeeDashboard.jsx
@@ -1,4 +1,5 @@
-import { CalendarIcon, DollarSignIcon, FileTextIcon } from "lucide-react"
+import { ArrowRightIcon, CalendarIcon, DollarSignIcon, FileTextIcon } from "lucide-react"
+import { Link } from "react-router-dom";
 
 const EmployeeDashboard = ({data}) => {
 
@@ -44,6 +45,17 @@ const EmployeeDashboard = ({data}) => {
                 </div>
             ))}
         </div>
+
+        <div className="flex flex-col sm:flex-row gap-3">
+            <Link to="/attendance" className="btn-primary text-center inline-flex items-center justify-center gap-2">
+                Mark Attendance <ArrowRightIcon className="w-4 h-4"/>
+            </Link>
+
+            <Link to="/leave" className="btn-secondary text-center">
+                Apply for Leave 
+            </Link>
+        </div>
+
     </div>
   )
 }
