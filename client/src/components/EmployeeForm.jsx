1719da60abfb98155086f3a00bbbe6edcbb0e4fe
200-hassan <md.arhassan@gmail.com>
2026-06-19 04:05:25 +0600

    create employee_details section in employee form

--- client/src/components/EmployeeForm.jsx
+++ client/src/components/EmployeeForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom"
+import { DEPARTMENTS } from "../assets/assets";
 
 const EmployeeForm = ({initialData, onSuccess, onCancel}) => {
 
@@ -41,6 +42,54 @@ const EmployeeForm = ({initialData, onSuccess, onCancel}) => {
             </div>
         </div>
 
+        {/* Employment Details*/}
+        <div className="card p-5 sm:p-6">
+            <h3 className="text-base font-medium text-slate-900 mb-6 pb-4 border-b border-slate-100">
+                Employment Details
+            </h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 text-sm text-slate-700">
+                <div>
+                    <label className="block mb-2">Department</label>
+                    <select name="department" defaultValue={initialData?.department || ""}>
+                        <option value="">Select Department</option>
+                        {DEPARTMENTS.map((deptName)=>(
+                            <option key={deptName} value={deptName}>
+                                {deptName}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    <label className="block mb-2">Position</label>
+                    <input name="position" required defaultValue={initialData?.position}/>
+                </div>
+                <div>
+                    <label className="block mb-2">Basic Salary</label>
+                    <input type="number" name="basicSalary" required min="0" step="0.01" defaultValue={initialData?.basicSalary || 0}/>
+                </div>
+                <div>
+                    <label className="block mb-2">Allowances</label>
+                    <input type="number" name="allowances" required min="0" step="0.01" defaultValue={initialData?.allowances || 0}/>
+                </div>
+                <div>
+                    <label className="block mb-2">Deductions</label>
+                    <input type="number" name="deductions" required min="0" step="0.01" defaultValue={initialData?.deductions || 0}/>
+                </div>
+                {isEditMode && (
+                    <div>
+                        <label className="block mb-2">Status</label>
+                        <select name="employmentStatus" defaultValue={initialData?.employmentStatus}>
+                            <option value="ACTIVE">Active</option>
+                            <option value="InACTIVE">Inactive</option>
+                        </select>
+                    </div>
+                )}
+            </div>
+        </div>
+
+        {/* Account Setup */}
+
+        {/* Button */}
 
     </form>
   )
