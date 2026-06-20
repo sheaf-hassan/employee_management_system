f1dac7b1695ef9c760d3b5948fbf9f09dca0716a
200-hassan <md.arhassan@gmail.com>
2026-06-21 02:23:56 +0600

    create the attendance page functionality

--- client/src/pages/Attendance.jsx
+++ client/src/pages/Attendance.jsx
@@ -1,5 +1,30 @@
+import { useCallback, useEffect, useState } from "react"
+import { dummyAttendanceData } from "../assets/assets";
+import Loading from "../components/Loading"
 
 const Attendance = () => {
+
+  const [history, setHistory] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const fetchData = useCallback(async ()=>{
+    setHistory(dummyAttendanceData)
+    setTimeout(()=>{
+      setLoading(false)
+    },1000)
+  },[]);
+
+  useEffect(()=>{
+    fetchData()
+  },[fetchData]);
+ 
+  if (loading) return <Loading />
+
+  const toady = new Date();
+  toady.setHours(0, 0, 0, 0)
+  const todayRecord = history.find((r)=> new Date(r.date).toDateString() === toady.toDateString());
+
   return (
     <div>Attendance</div>
   )
