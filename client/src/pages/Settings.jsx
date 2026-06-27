d2fb22b7998a194dbc3bbaf8d1be92dcd933e473
200-hassan <md.arhassan@gmail.com>
2026-06-28 03:31:26 +0600

    create the settings page functionality

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react"
+import { dummyProfileData } from "../assets/assets";
+import Loading from "../components/Loading";
 
 const Settings = () => {
+
+  const [profile, setProfile] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [showPasswordModal, setShowPasswordModal] = useState(false);
+
+  const fetchProfile = async ()=> {
+    setProfile(dummyProfileData);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  };
+
+  useEffect(()=>{
+    fetchProfile()
+  },[]);
+
+  if(loading) return <Loading />
+
   return (
     <div>Settings</div>
   )
