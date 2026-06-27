9340952e38bbaf92ec95f8c51f1c757aaa78e45a
200-hassan <md.arhassan@gmail.com>
2026-06-27 23:48:03 +0600

    create payslip_list and mount it to the payslips page

--- client/src/pages/Payslips.jsx
+++ client/src/pages/Payslips.jsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 import { dummyEmployeeData, dummyPayslipData } from "../assets/assets";
 import Loading from "../components/Loading";
+import PayslipList from "../components/payslip/PayslipList";
 
 const Payslips = () => {
   const [payslips, setPayslips] = useState([]);
@@ -27,7 +28,7 @@ const Payslips = () => {
 
   return (
     <div className="animate-fade-in">
-      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb--8">
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-8">
         <div>
           <h1 className="page-title">Payslips</h1>
           <p className="page-subtitle">
@@ -38,7 +39,7 @@ const Payslips = () => {
         </div>
         {isAdmin && <p>GENERATE FORM</p>}
       </div>
-      <p>Payslips list</p>
+      <PayslipList payslips={payslips} isAdmin={isAdmin}/>
     </div>
   );
 };
