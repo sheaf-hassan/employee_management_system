adfd9e9d20bafc90aed4841298ee4bcf0e5badde
200-hassan <md.arhassan@gmail.com>
2026-06-27 21:46:56 +0600

    create the payslips page functionality

--- client/src/pages/Payslips.jsx
+++ client/src/pages/Payslips.jsx
@@ -1,8 +1,46 @@
+import { useCallback, useEffect, useState } from "react";
+import { dummyEmployeeData, dummyPayslipData } from "../assets/assets";
+import Loading from "../components/Loading";
 
 const Payslips = () => {
-  return (
-    <div>Payslips</div>
-  )
-}
+  const [payslips, setPayslips] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const isAdmin = true;
 
-export default Payslips
+  const fetchPaySlips = useCallback(async () => {
+    setPayslips(dummyPayslipData);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    fetchPaySlips();
+  }, [fetchPaySlips]);
+
+  useEffect(() => {
+    if (isAdmin) setEmployees(dummyEmployeeData);
+  }, [isAdmin]);
+
+  if (loading) return <Loading />;
+
+  return (
+    <div className="animate-fade-in">
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb--8">
+        <div>
+          <h1 className="page-title">Payslips</h1>
+          <p className="page-subtitle">
+            {isAdmin
+              ? "Generate and manage employee payslips"
+              : "Your payslip history"}
+          </p>
+        </div>
+        {isAdmin && <p>GENERATE FORM</p>}
+      </div>
+      <p>Payslips list</p>
+    </div>
+  );
+};
+
+export default Payslips;
