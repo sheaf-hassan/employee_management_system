646ed61f7e6bcde52bbdb5f3197f3627e842d187
200-hassan <md.arhassan@gmail.com>
2026-06-28 16:30:07 +0600

    create the basic back_end server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import multer from "multer";
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+app.use(multer().none())
+
+// Routes
+app.get("/", (req, res)=> res.send("Server is running"))
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
