ac5a77461285078518330df6a4f0438b40f6bca5
200-hassan <md.arhassan@gmail.com>
2026-06-30 04:09:15 +0600

    create get_session and change_password auth_controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -45,3 +45,40 @@ export const login = async (req, res) => {
         return res.status(500).json({error: "Login failed"});
     }
 }
+
+// Get session for employee and admin
+// GET /api/auth/session
+export const session = (req, res)=>{
+    const session = req.session;
+    return res.json({user: session})
+}
+
+// Change password for employee and admin
+// POST /api/auth/change-password
+export const changePassword = async (req, res)=>{
+    try {
+        const session = req.session;
+        const {currentPassword, newPassword} = req.body;
+
+        if(!currentPassword || !newPassword){
+            return res.status(400).json({error: "Both passwords are  required"});
+        };
+
+        const user = await User.findById(session.userId);
+
+        if(!user) return res.status(404).json({error: "User not found"});
+
+        const isValid = await bcrypt.compare(currentPassword, user.password);
+
+        if(!isValid) return res.status(400).json({error: "Current password is incorrect"});
+
+        const hashed = await bcrypt.hash(newPassword, 10);
+
+        await User.findByIdAndUpdate(session.userId, {password: hashed})
+
+        return res.json({success: true});
+
+    } catch (error) {
+        return res.status(500).json({error: "Failed to change password"})
+    }
+}
