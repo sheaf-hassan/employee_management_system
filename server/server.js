718eb56cb5f940f742b909cf9c434009cec4f864
200-hassan <md.arhassan@gmail.com>
2026-06-30 05:59:37 +0600

    integrate auth_routes and employee_routes with the server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import multer from "multer";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import employeesRouter from "./routes/employeeRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -13,7 +15,9 @@ app.use(express.json())
 app.use(multer().none())
 
 // Routes
-app.get("/", (req, res)=> res.send("Server is running"))
+app.get("/", (req, res)=> res.send("Server is running"));
+app.use("/api/auth", authRouter);
+app.use("/api/employees", employeesRouter)
 
 await connectDB()
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
