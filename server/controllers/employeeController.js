7104aa5238609d288d9135ab635a90d1dba3afc5
200-hassan <md.arhassan@gmail.com>
2026-06-29 16:34:06 +0600

    create create_employee function

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -1,4 +1,7 @@
 import Employee from "../models/Employee.js";
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
+import Employee from "../models/Employee.js";
 
 // Get employees
 // GET /api/employees
@@ -26,7 +29,45 @@ export const getEmployees = async (req, res)=>{
 // Create employee
 // POST /api/employees
 export const createEmployee = async (req, res)=>{
+    try {
+        const {firstName, lastName, email, phone, position, department, basicSalary, allowances, deductions, joinDate, password, role, bio} = req.body;
 
+        if(!email || !password || !firstName || !lastName){
+            return res.status(400).json({error: "Missing required fields"});
+        }
+
+        const hashed = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            email,
+            password: hashed,
+            role: role || "EMPLOYEE"
+        })
+
+        const employee = await Employee.create({
+            userId: user._id,
+            firstName,
+            lastName,
+            email,
+            phone,
+            position,
+            department: department || "Engineering",
+            basicSalary: Number(basicSalary) || 0,
+            allowances: Number(allowances) || 0,
+            deductions: Number(deductions) || 0,
+            joinDate: new Date(joinDate),
+            bio: bio || "",
+        })
+
+        return res.status(201).json({success: true, employee})
+
+    } catch (error) {
+        if(error.code === 11000){
+            return res.status(400).json({error: "Email already exists"})
+        }
+        console.error("Create employee error", error)
+
+        return res.status(500).json({error: "Failed to create employee"});
+    }
 }
 
 // Update employee
