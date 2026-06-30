61386eb37103a473ed49ffa35d327bb55d8b9c1f
200-hassan <md.arhassan@gmail.com>
2026-07-01 02:02:54 +0600

    fix errors in attendace model

--- server/models/Attendance.js
+++ server/models/Attendance.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const attendanceSchema = new mongoose.Schema({
     employeeId: {type: mongoose.Schema.Types.ObjectId, ref:"Employee", required: true},
     date: {type: Date, required: true},
-    checkIn: {type: date, default: null},
-    checkOut: {type: date, default: null},
+    checkIn: {type: Date, default: null},
+    checkOut: {type: Date, default: null},
     status: {type: String, enum: ["PRESENT", "ABSENT", "LATE"], default: "PRESENT"},
     workingHours: {type: Number, default: null},
     dayType: {type: String, enum: ["Full Day", "Three Quarter Day", "Half Day", "Short Day", null], default: null}
