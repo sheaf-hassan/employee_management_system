ae629d3acc4a708300298bd8c25719c98503a78c
200-hassan <md.arhassan@gmail.com>
2026-07-01 02:46:33 +0600

    fix some bugs

--- server/controllers/attendanceController.js
+++ server/controllers/attendanceController.js
@@ -69,7 +69,7 @@ export  const getAttendance = async(req, res) => {
         if (!employee) return res.status(404).json({error: "Employee not found"});
 
         const limit = parseInt(req.query.limit || 30);
-        const history = (await Attendance.find({employeeId: employee._id})).sort({date: -1}).limit(limit);
+        const history = await Attendance.find({employeeId: employee._id}).sort({date: -1}).limit(limit);
 
         return res.json({
             data: history,
--- server/models/Attendance.js
+++ server/models/Attendance.js
@@ -13,6 +13,6 @@ const attendanceSchema = new mongoose.Schema({
 
 attendanceSchema.index({employeeId: 1, date: 1}, {unique: true})
 
-const Attendance = mongoose.models.User || mongoose.model("Attendance", attendanceSchema)
+const Attendance = mongoose.models.Attendance || mongoose.model("Attendance", attendanceSchema)
 
 export default Attendance;
