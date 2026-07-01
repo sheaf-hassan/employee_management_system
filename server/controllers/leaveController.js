cbd698e7ff8c3fe73e7b1545cc6e4a6c958c2aa3
200-hassan <md.arhassan@gmail.com>
2026-07-01 21:44:40 +0600

    create update_leave_status function

--- server/controllers/leaveController.js
+++ server/controllers/leaveController.js
@@ -87,8 +87,19 @@ export const getLeaves = async (req, res) => {
     }
 }
 
-// Update leave
-// PATCH /api/leaves
-export const updateLeaves = async (req, res) => {
+// Update leave status
+// PATCH /api/leaves/:id
+export const updateLeaveStatus = async (req, res) => {
+    try {
+        const { status } = req.body;
+        if (!["APPROVED", "REJECTED", "PENDING"].includes(status)){
+            return res.status(400).json({error: "Invalid status"});
+        }
 
+        const leave = await LeaveApplication.findByIdAndUpdate(req.params.id, {status}, {returnDocument: "after"});
+
+        return res.json({success: true, data: leave})
+    } catch (error) {
+        return res.status(500).json({error: "Failed"});
+    }
 }
