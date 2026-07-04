9d3f8f51bc7387c4bae7dd4b305b16f8b9c7f9b5
200-hassan <md.arhassan@gmail.com>
2026-07-04 16:09:28 +0600

    create payslips_routes and integrate it with the server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import employeesRouter from "./routes/employeeRoutes.js";
 import profileRouter from "./routes/profileRoutes.js";
 import attendanceRouter from "./routes/attendanceRoutes.js";
 import leaveRouter from "./routes/leaveRoutes.js";
+import payslipRouter from "./routes/payslipsRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -24,6 +25,7 @@ app.use("/api/employees", employeesRouter);
 app.use("/api/profile", profileRouter);
 app.use("/api/attendance", attendanceRouter);
 app.use("/api/leave", leaveRouter);
+app.use("/api/payslips", payslipRouter);
 
 await connectDB()
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
