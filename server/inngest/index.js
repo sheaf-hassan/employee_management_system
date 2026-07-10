36eafc4271ec16d4665d797b965956873c588f06
200-hassan <md.arhassan@gmail.com>
2026-07-11 00:36:22 +0600

    fixed email send cron for attendance

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -160,10 +160,12 @@ const attendanceReminderCron = inngest.createFunction(
                             `
                     })
                 })
+                await Promise.all(emailPromises)
+                return {emailSent: absentEmployees.length}
             })
         }
 
-        await Promise.all(emailPromises)
+        
         return {totalActive: activeEmployees.length, onLeave: onLeaveIds.length, checkedIn: checkedInIds.length, absent: absentEmployees.length}
     }
 )
